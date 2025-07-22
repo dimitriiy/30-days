20b48a92651bd86513c9df8a15fef55fcbfa46aa
Dmitry <mazitov.dima@mail.ru>
2025-07-22 23:46:26 +0300

    Update challenge.config.js

--- challenge.config.js
+++ challenge.config.js
@@ -1,6 +1,6 @@
 export default {
-  startDate: new Date(2025, 2, 24, 0, 0),
-  endDate: new Date(2025, 3, 24, 0, 0),
-  version: '6.0',
-  dateTitle: 'Март - Апрель',
+  startDate: new Date(2025, 6, 21, 0, 0),
+  endDate: new Date(2025, 7, 21, 0, 0),
+  version: '7.0',
+  dateTitle: 'Июль - Август',
 };
